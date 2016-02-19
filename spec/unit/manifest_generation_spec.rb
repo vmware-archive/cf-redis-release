ef27f10146bedc794cbee11044a54627003b2da7
Claudia Beresford <cberesford@pivotal.io>
2016-02-19 17:00:56 +0000

    Ability to add extra releases on the fly
    [#112665957]
    
    Signed-off-by: Jamie Monserrate <jmonserrate@pivotal.io>

--- spec/unit/manifest_generation_spec.rb
+++ spec/unit/manifest_generation_spec.rb
@@ -16,7 +16,7 @@ describe 'spiff manifests' do
     let(:stub) { 'templates/sample_stubs/sample_warden_stub.yml' }
     let(:infrastructure) { 'warden' }
 
-    it 'should do stuff' do
+    it 'should generate manifest' do
       example_manifest = Tempfile.new("example-manifest.yml")
 
       @clazz.process("scripts/generate_deployment_manifest #{infrastructure} #{stub} > #{example_manifest.path}", expected_exit_status: 0)
@@ -24,6 +24,23 @@ describe 'spiff manifests' do
       expected = File.read("spec/fixtures/cf-redis-#{infrastructure}.yml")
       actual = File.read(example_manifest.path)
       expect(actual).to yaml_eq(expected)
+    end
+
+    it 'should allow users to override the jobs section' do
+      custom_jobs_file = Tempfile.new("custom-jobs.yml")
+      custom_jobs_file.write({
+            'additional_releases' => [
+              {'name' => 'another-redis-release', 'version' => 'latest'},
+              {'name' => 'my-custom-release', 'version' => 'latest'}]}.to_yaml)
+      custom_jobs_file.close
+      example_manifest = Tempfile.new("example-manifest.yml")
+
+      @clazz.process("scripts/generate_deployment_manifest #{infrastructure} #{stub} #{custom_jobs_file.path} > #{example_manifest.path}", expected_exit_status: 0)
+
+      actual = YAML.load(File.read(example_manifest.path))
+      expect(actual['releases']).to include({ 'name' => 'cf-redis', 'version' => 'latest'})
+      expect(actual['releases']).to include({ 'name' => 'another-redis-release', 'version' => 'latest'})
+      expect(actual['releases']).to include({ 'name' => 'my-custom-release', 'version' => 'latest'})
     end
   end
 end
