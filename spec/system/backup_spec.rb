6fc5bae12929650b50ec6d56bd412198fba0d52e
Simon Jones <simonjones@pivotal.io>
2016-02-23 15:37:36 +0000

    End to end test of manually triggered backup
    [#113080569]
    
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

--- spec/system/backup_spec.rb
+++ spec/system/backup_spec.rb
@@ -8,17 +8,39 @@ describe 'backups' do
 
   let(:redis_config_command) { bosh_manifest.property("redis.config_command") }
   let(:broker_vm_ip) { bosh_director.ips_for_job('cf-redis-broker', bosh_manifest.deployment_name).first }
+  let(:aws_access_key_id) { bosh_manifest.property("service-backup.destination.s3.access_key_id") }
+  let(:aws_secret_access_key) { bosh_manifest.property("service-backup.destination.s3.secret_access_key") }
 
   let(:s3_client) {
     AWS::S3.new(
-      access_key_id: bosh_manifest.property("redis.broker.backups.access_key_id"),
-      secret_access_key: bosh_manifest.property("redis.broker.backups.secret_access_key")
+      access_key_id: aws_access_key_id,
+      secret_access_key: aws_secret_access_key
     )
   }
-  let(:s3_backup_bucket) { bosh_manifest.property("redis.broker.backups.bucket_name") }
+  let(:s3_backup_bucket) { bosh_manifest.property("service-backup.destination.s3.bucket_name") }
+  let(:s3_backup_path) { bosh_manifest.property("service-backup.destination.s3.bucket_path") }
 
   let(:test_key) { "test_key" }
   let(:test_value) { "test_value" }
+
+  let(:destination_folder) { bosh_manifest.property("service-backup.source_folder") }
+
+  let(:manual_backup_command) do
+    "/var/vcap/packages/service-backup/bin/manual-backup s3 " +
+      "--backup-creator-cmd '#{manual_snapshot_command}' " +
+      "--source-folder '#{destination_folder}' " +
+      "--cleanup-cmd '' " +
+      "--dest-path '#{s3_backup_path}' " +
+      "--aws-access-key-id '#{aws_access_key_id}' " +
+      "--aws-secret-access-key '#{aws_secret_access_key}' " +
+      "--aws-cli-path /var/vcap/packages/aws-cli/bin/aws " +
+      "--dest-bucket '#{s3_backup_bucket}' " +
+      "--endpoint-url 'https://s3.amazonaws.com/' "
+  end
+  let(:manual_snapshot_command) do
+    '/var/vcap/packages/redis-backups/bin/snapshot ' +
+      '-config /var/vcap/jobs/redis-backups/config/backup-config.yml'
+  end
 
   context "shared vm plan" do
     let(:redis_save_command) { bosh_manifest.property("redis.save_command") }
@@ -30,14 +52,13 @@ describe 'backups' do
           plan: 'shared-vm'
         )
       }
-      let(:destination_folder) { bosh_manifest.property("service-backup.source_folder") }
 
       it 'creates a dump.rdb file' do
         service_broker.provision_and_bind(service.name, service.plan) do |service_binding, service_instance|
           vm_ip = service_binding.credentials[:host]
           result = ssh_gateway.execute_on(
             vm_ip,
-            '/var/vcap/packages/redis-backups/bin/snapshot -config /var/vcap/jobs/redis-backups/config/backup-config.yml'
+            manual_snapshot_command
           )
           expect(result.lines.join).to(match('"event":"done","task":"create-snapshot"'), 'done event not found')
 
@@ -141,30 +162,59 @@ describe 'backups' do
       expect(crontab_output).to include(expected)
     end
 
-    describe 'manual snapshot', :skip_service_backups => true do
+    describe 'service backups', :skip_service_backups => true do
       let(:service) {
         Prof::MarketplaceService.new(
           name: bosh_manifest.property('redis.broker.service_name'),
           plan: 'dedicated-vm'
         )
       }
-      let(:destination_folder) { bosh_manifest.property("service-backup.source_folder") }
 
-      it 'creates a dump.rdb file' do
-        service_broker.provision_and_bind(service.name, service.plan) do |service_binding, service_instance|
-          vm_ip = service_binding.credentials[:host]
-          result = ssh_gateway.execute_on(
-            vm_ip,
-            '/var/vcap/packages/redis-backups/bin/snapshot -config /var/vcap/jobs/redis-backups/config/backup-config.yml'
-          )
-          expect(result.lines.join).to(match('"event":"done","task":"create-snapshot"'), 'done event not found')
+      describe 'manual snapshot' do
+        let(:destination_folder) { bosh_manifest.property("service-backup.source_folder") }
 
-          ls_output = ssh_gateway.execute_on(
-            vm_ip, "ls -l #{destination_folder}dump.rdb"
-          )
-          expect(ls_output.lines.size).to(eql(1))
-          expect(ls_output.lines.first).to_not(match('No such file or directory'))
-          expect(ls_output.lines.first).to(match("#{destination_folder}dump.rdb"))
+        it 'creates a dump.rdb file' do
+          service_broker.provision_and_bind(service.name, service.plan) do |service_binding, service_instance|
+            vm_ip = service_binding.credentials[:host]
+            result = ssh_gateway.execute_on(
+              vm_ip,
+              manual_snapshot_command
+            )
+            expect(result.lines.join).to(match('"event":"done","task":"create-snapshot"'), 'done event not found')
+
+            ls_output = ssh_gateway.execute_on(
+              vm_ip, "ls -l #{destination_folder}dump.rdb"
+            )
+            expect(ls_output.lines.size).to(eql(1))
+            expect(ls_output.lines.first).to_not(match('No such file or directory'))
+            expect(ls_output.lines.first).to(match("#{destination_folder}dump.rdb"))
+          end
+        end
+      end
+
+      describe 'manual backup' do
+        before do
+          instance_id = create_backup_for_service(service)
+
+          @s3_backup_file = s3_client.buckets[s3_backup_bucket].objects.
+            find_all {|object| object.key.include? "backup/#{Time.now.strftime("%Y/%m/%d")}"}.
+            find { |object| "dump.rdb" }
+        end
+
+        after do
+          @s3_backup_file.delete if @s3_backup_file
+        end
+
+        it 'uploads data to S3 in RDB format' do
+          expect(@s3_backup_file.exists?).to eq(true)
+          expect(@s3_backup_file.content_length).not_to eq(0)
+          contents = @s3_backup_file.read
+
+          # check RDB format
+          expect(contents).to match(/^REDIS/)
+
+          # check not AOF format
+          expect(contents).to_not include('SELECT')
         end
       end
     end
@@ -235,8 +285,8 @@ describe 'backups' do
       client.run(redis_save_command)
 
       with_redis_under_stress(service_binding) do
-        result = ssh_gateway.execute_on(vm_ip, backup_command_from_crontab(vm_ip))
-        expect(result.lines.last.strip).to(eql('0'), 'backup command failed with non zero exit status')
+        result = ssh_gateway.execute_on(vm_ip, manual_backup_command)
+        expect(result.lines.join).to(match(/Upload backup completed without error/))
       end
 
       return service_instance.id
