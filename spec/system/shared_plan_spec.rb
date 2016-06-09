5c491d9f16423d57ae7d2e6c81b424d172e66ebb
Jake Klein <jakek95@live.co.uk>
2016-06-09 16:12:48 +0100

    Refactor service binding
    [#120693729]
    
    Signed-off-by: Jonathan Qiwen Tsang <jtsang@pivotal.io>

--- spec/system/shared_plan_spec.rb
+++ spec/system/shared_plan_spec.rb
@@ -17,14 +17,14 @@ describe 'shared plan' do
   let(:manually_drain) { '/var/vcap/jobs/cf-redis-broker/bin/drain' }
 
   it 'preserves data when recreating vms' do
-    service_broker.provision_and_bind(service.name, service.plan) do |binding|
-      service_client = service_client_builder(binding)
+    service_broker.provision_and_bind(service.name, service.plan) do | service_binding |
+      service_client = service_client_builder(service_binding)
       service_client.write('test_key', 'test_value')
       expect(service_client.read('test_key')).to eq('test_value')
 
       # TODO do not manually run drain once bosh bug fixed
       bosh_director.stop(environment.bosh_service_broker_job_name, 0)
-      host = binding.credentials.fetch(:host)
+      host = service_binding.credentials.fetch(:host)
       ssh_gateway.execute_on(host, manually_drain, root: true)
 
       bosh_director.recreate_all([environment.bosh_service_broker_job_name])
@@ -40,26 +40,26 @@ describe 'shared plan' do
   describe 'redis configuration' do
     before(:all) do
       @service_instance = service_broker.provision_instance(service.name, service.plan)
-      @binding          = service_broker.bind_instance(@service_instance)
+      @service_binding  = service_broker.bind_instance(@service_instance)
     end
 
     after(:all) do
-      service_broker.unbind_instance(@binding)
+      service_broker.unbind_instance(@service_binding)
       service_broker.deprovision_instance(@service_instance)
     end
 
     it 'has the correct maxclients' do
-      service_client = service_client_builder(@binding)
+      service_client = service_client_builder(@service_binding)
       expect(service_client.config.fetch('maxclients')).to eq("10000")
     end
 
     it 'has the correct maxmemory' do
-      service_client = service_client_builder(@binding)
+      service_client = service_client_builder(@service_binding)
       expect(service_client.config.fetch('maxmemory').to_i).to eq(maxmemory)
     end
 
     it 'runs correct version of redis' do
-      service_client = service_client_builder(@binding)
+      service_client = service_client_builder(@service_binding)
       expect(service_client.info('redis_version')).to eq('3.0.7')
     end
   end
@@ -76,15 +76,15 @@ describe 'shared plan' do
     end
 
     it 'updates existing instances' do
-      service_broker.provision_and_bind(service.name, service.plan) do |binding|
-        redis_client_1 = service_client_builder(binding)
+      service_broker.provision_and_bind(service.name, service.plan) do | service_binding |
+        redis_client_1 = service_client_builder(service_binding)
         redis_client_1.write('test', 'foobar')
         original_config_command = redis_client_1.config_command
 
         bosh_manifest.set_property('redis.config_command', 'newconfigalias')
         bosh_director.deploy
 
-        redis_client_2 = service_client_builder(binding)
+        redis_client_2 = service_client_builder(service_binding)
         new_config_command = redis_client_2.config_command
         expect(original_config_command).to_not eq(new_config_command)
         expect(redis_client_2.read('test')).to eq('foobar')
