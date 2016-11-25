978456151ccb814351ca66f6945b303fef74677f
Maria Ntalla <mntalla@pivotal.io>
2016-11-25 14:27:50 +0000

    Updated expected ciphers in security_spec
    [#134350665]
    
    Signed-off-by: Jake Klein <jklein@pivotal.io>

--- spec/system/security_spec.rb
+++ spec/system/security_spec.rb
@@ -26,7 +26,10 @@ describe 'security' do
 
     it 'only supports HTTPS with restricted ciphers' do
       agent_url = "https://#{node_hosts.first}"
-      expect(agent_url).to only_support_ssl_with_cipher_set(Prof::SSL::CipherSet::PIVOTAL_MODERN).with_proxy(environment.http_proxy)
+      supported_ciphers = ["DHE-RSA-AES128-GCM-SHA256", "DHE-RSA-AES256-GCM-SHA384", "ECDHE-RSA-AES128-GCM-SHA256", "ECDHE-RSA-AES256-GCM-SHA384"]
+      supported_protocols = [:TLSv1_2]
+      cipher_set = Prof::SSL::CipherSet::new(supported_ciphers:supported_ciphers, supported_protocols:supported_protocols)
+    expect(agent_url).to only_support_ssl_with_cipher_set(cipher_set).with_proxy(environment.http_proxy)
     end
 
     it 'does not listen publicly on the backend_port' do
