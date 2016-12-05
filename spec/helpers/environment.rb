3c9a8d23d9f99af526bc6c67c2ef7f3fce31f864
Tom Godkin <tgodkin@pivotal.io>
2016-12-05 15:59:37 +0000

    Ignore deprecation warning for timeout
    [#134350661]
    
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

--- spec/helpers/environment.rb
+++ spec/helpers/environment.rb
@@ -3,6 +3,20 @@ require 'prof/environment/cloud_foundry'
 require 'support/redis_service_broker'
 require 'support/redis_service_client_builder'
 
+class FilteredStderr < StringIO
+  def write value
+    if value.include? "Object#timeout is deprecated"
+      return
+    end
+
+    if value == "\n"
+      return
+    end
+
+    STDERR.write value
+  end
+end
+
 module Helpers
   module Environment
     fail "Must specify BOSH_MANIFEST environment variable" unless ENV.key?('BOSH_MANIFEST')
@@ -41,8 +55,22 @@ module Helpers
       environment.bosh_director
     end
 
+    # net-ssh makes a deprecated call to `timeout`. We ignore these messages
+    # because they pollute logs.
+    # After using the filtered stderr we ensure to reassign the original stderr
+    # stream.
     def ssh_gateway
-      environment.ssh_gateway
+      gateway = environment.ssh_gateway
+      def gateway.execute_on(*args, &block)
+        begin
+          original_stderr = $stderr
+          $stderr = FilteredStderr.new
+          super
+        ensure
+          $stderr = original_stderr
+        end
+      end
+      gateway
     end
 
     def broker_host
