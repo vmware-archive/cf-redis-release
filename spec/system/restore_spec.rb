f4facad872a2ece352768fc0f366ef329400c8bb
Maria Ntalla <mntalla@pivotal.io>
2016-12-16 16:26:08 +0000

    Add system tests for fail-fast restore scenarios
    They cover:
    - not running as root
    - providing a sourceRDB in a different device from /var/vcap/store
    - providing a wrong service instance guid
    
    [#135939137]
    
    Signed-off-by: Jake Klein <jklein@pivotal.io>

--- spec/system/restore_spec.rb
+++ spec/system/restore_spec.rb
@@ -2,7 +2,7 @@ require 'system_spec_helper'
 require 'httparty'
 
 RESTORE_BINARY = '/var/vcap/jobs/redis-backups/bin/restore'
-BACKUP_PATH = '/var/vcap/store/redis-backup/dump.rdb'
+BACKUP_PATH = '/var/vcap/store/dump.rdb'
 
 shared_examples 'it can restore Redis' do |plan|
   before(:all) do
@@ -15,7 +15,7 @@ shared_examples 'it can restore Redis' do |plan|
     stage_dump_file @vm_ip
     expect(@client.read("moaning")).to_not eq("myrtle")
 
-    restore_args = get_restore_args plan, @service_instance.id
+    restore_args = get_restore_args plan, @service_instance.id, BACKUP_PATH
     root_execute_on(@vm_ip, "#{RESTORE_BINARY} #{restore_args}")
   end
 
@@ -31,10 +31,108 @@ shared_examples 'it can restore Redis' do |plan|
   end
 
   it 'logs successful completion of restore' do
-    vm_log = root_execute_on(@vm_ip, "cat /var/vcap/sys/log/redis-backups/redis-backups.log")
+    vm_log = root_execute_on(@vm_ip, "cat /var/vcap/sys/log/service-backup/restore.log")
     contains_expected_log = drop_log_lines_before(@preprovision_timestamp, vm_log).any? do |line|
       line.include?('"restore.LogRestoreComplete","log_level":1,"data":{"message":"Redis data restore completed successfully"}}')
     end
+
+    expect(contains_expected_log).to be true
+  end
+end
+
+shared_examples 'it errors because of non-root user' do |plan|
+  before(:all) do
+    @service_instance, @service_binding = provision_and_bind plan
+
+    @vm_ip = @service_binding.credentials[:host]
+    @preprovision_timestamp = root_execute_on(@vm_ip, "date +%s")
+    @client = service_client_builder(@service_binding)
+    expect(@client.read("moaning")).to_not eq("myrtle")
+    restore_args = get_restore_args plan, @service_instance.id, BACKUP_PATH
+    ssh_gateway.execute_on(@vm_ip, "#{RESTORE_BINARY} #{restore_args}")
+  end
+
+  after(:all) do
+    expect(broker_registered?).to be true
+
+    service_broker.unbind_instance(@service_binding)
+    service_broker.deprovision_instance(@service_instance)
+  end
+
+  it 'logs that restore should be run as root' do
+    vm_log = root_execute_on(@vm_ip, "cat /var/vcap/sys/log/service-backup/restore.log")
+    contains_expected_log = drop_log_lines_before(@preprovision_timestamp, vm_log).any? do |line|
+      line.include?('"restore.CheckPreconditions","log_level":2,"data":{"error":"expected the script to be running as user `root`, got `vcap`","username":"vcap"}}')
+    end
+
+    expect(contains_expected_log).to be true
+  end
+end
+
+shared_examples 'it errors because of file on wrong device' do |plan|
+  before(:all) do
+    @service_instance, @service_binding = provision_and_bind plan
+
+    @vm_ip = @service_binding.credentials[:host]
+    @preprovision_timestamp = root_execute_on(@vm_ip, "date +%s")
+    @client = service_client_builder(@service_binding)
+
+    stage_dump_file_incorrectly @vm_ip
+    expect(@client.read("moaning")).to_not eq("myrtle")
+
+    tmp_backup_path = '/tmp/moaning-dump.rdb'
+    restore_args = get_restore_args plan, @service_instance.id, tmp_backup_path
+    root_execute_on(@vm_ip, "#{RESTORE_BINARY} #{restore_args}")
+  end
+
+  after(:all) do
+    root_execute_on(@vm_ip, 'rm /tmp/moaning-dump.rdb')
+    expect(broker_registered?).to be true
+
+    service_broker.unbind_instance(@service_binding)
+    service_broker.deprovision_instance(@service_instance)
+  end
+
+  it 'logs that the file should be in /var/vcap/store' do
+
+    vm_log = root_execute_on(@vm_ip, "cat /var/vcap/sys/log/service-backup/restore.log")
+    contains_expected_log = drop_log_lines_before(@preprovision_timestamp, vm_log).any? do |line|
+      line.include?('restore.CheckPreconditions","log_level":2,"data":{"error":"Please move your rdb file to inside /var/vcap/store","path":"/tmp/moaning-dump.rdb"}')
+    end
+    expect(contains_expected_log).to be true
+  end
+end
+
+shared_examples 'it errors because of an incorrect guid' do |plan|
+  before(:all) do
+    @service_instance, @service_binding = provision_and_bind plan
+
+    @vm_ip = @service_binding.credentials[:host]
+    @preprovision_timestamp = root_execute_on(@vm_ip, "date +%s")
+    @client = service_client_builder(@service_binding)
+
+    stage_dump_file @vm_ip
+    expect(@client.read("moaning")).to_not eq("myrtle")
+
+    restore_args = "--sourceRDB #{BACKUP_PATH} --sharedVmGuid imafakeguid"
+    root_execute_on(@vm_ip, "#{RESTORE_BINARY} #{restore_args}")
+  end
+
+  after(:all) do
+    root_execute_on(@vm_ip, 'rm /tmp/moaning-dump.rdb')
+    expect(broker_registered?).to be true
+
+    service_broker.unbind_instance(@service_binding)
+    service_broker.deprovision_instance(@service_instance)
+  end
+
+  it 'logs that the file should be in /var/vcap/store' do
+
+    vm_log = root_execute_on(@vm_ip, "cat /var/vcap/sys/log/service-backup/restore.log")
+    contains_expected_log = drop_log_lines_before(@preprovision_timestamp, vm_log).any? do |line|
+      line.include?('restore.CheckPreconditions","log_level":2,"data":{"error":"service-instance provided does not exist, please check you are on the correct VM and the instance guid is correct')
+    end
+    expect(contains_expected_log).to be true
   end
 end
 
@@ -45,6 +143,21 @@ describe 'restore' do
 
   context 'dedicated-vm' do
     it_behaves_like 'it can restore Redis', 'dedicated-vm'
+  end
+
+  context 'not running as root' do
+    it_behaves_like 'it errors because of non-root user', 'shared-vm'
+    it_behaves_like 'it errors because of non-root user', 'dedicated-vm'
+  end
+
+  context 'source RDB not under /var/vcap/store' do
+    it_behaves_like 'it errors because of file on wrong device', 'shared-vm'
+    it_behaves_like 'it errors because of file on wrong device', 'dedicated-vm'
+  end
+
+  context 'source RDB not under /var/vcap/store' do
+    it_behaves_like 'it errors because of an incorrect guid', 'shared-vm'
+    it_behaves_like 'it errors because of an incorrect guid', 'dedicated-vm'
   end
 end
 
@@ -76,12 +189,19 @@ def stage_dump_file vm_ip
   root_execute_on(vm_ip, "mv /tmp/moaning-dump.rdb #{BACKUP_PATH}")
 end
 
-def get_restore_args plan, instance_id
-  restore_args = "--sourceRDB #{BACKUP_PATH}"
+def stage_dump_file_incorrectly vm_ip
+  local_dump = 'spec/fixtures/moaning-dump.rdb'
+  ssh_gateway.scp_to(vm_ip, local_dump, '/tmp/moaning-dump.rdb')
+end
 
-  if plan == 'shared-vm' then
+def get_restore_args plan, instance_id, source_rdb
+  restore_args = "--sourceRDB #{source_rdb}"
+
+  if plan == 'shared-vm'
     restore_args = "#{restore_args} --sharedVmGuid #{instance_id}"
   end
+
+  restore_args
 end
 
 def provision_and_bind plan
