fe723ba199637415967d7f5f1f56ae127c6f4b2e
Dan Wendorf <github@danwendorf.com>
2017-04-18 17:17:35 +0100

    Expose redis.config_command as a bosh link
    For both the broker and the dedicated-node
    
    [#143857833]
    
    Signed-off-by: Tom Godkin <tgodkin@pivotal.io>

---
+++ spec/unit/bosh_specs_spec.rb
@@ -0,0 +1,35 @@
+require 'yaml'
+
+PROJECT_ROOT = File.join(File.dirname(__FILE__), '..', '..')
+BROKER_SPEC_PATH = File.join(PROJECT_ROOT, 'jobs/cf-redis-broker/spec')
+DEDICATED_NODE_SPEC_PATH = File.join(PROJECT_ROOT, 'jobs/dedicated-node/spec')
+
+describe 'bosh specs' do
+  describe 'broker' do
+    subject { YAML.load_file(BROKER_SPEC_PATH) }
+
+    it 'is configured expose redis.config_command as a bosh link' do
+      expected_link = {
+        'name' => 'redis-broker',
+        'type' => 'redis',
+        'properties' => ['redis.config_command'],
+      }
+
+      expect(subject['provides']).to include(expected_link)
+    end
+  end
+
+  describe 'dedicated-node' do
+    subject { YAML.load_file(DEDICATED_NODE_SPEC_PATH) }
+
+    it 'is configured expose redis.config_command as a bosh link' do
+      expected_link = {
+        'name' => 'dedicated-node',
+        'type' => 'redis',
+        'properties' => ['redis.config_command'],
+      }
+
+      expect(subject['provides']).to include(expected_link)
+    end
+  end
+end
