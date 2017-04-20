2ccdaf24184f560921d15dd9eb4bee1682c84cab
Hannes Hoerl <hhorl@pivotal.io>
2017-04-20 16:17:35 +0100

    Revert "Bump redis to latest stable version 3.2.8"
    [#143964763]
    This reverts commit 08b9a5a7b75483ff5a08de151204bf05ee8cf2f0.
    
    Signed-off-by: Alvaro Perez-Shirley <aperezshirley@pivotal.io>

--- spec/system/dedicated_plan_spec.rb
+++ spec/system/dedicated_plan_spec.rb
@@ -82,7 +82,7 @@ describe 'dedicated plan' do
 
       it 'runs correct version of redis' do
         client = service_client_builder(@binding)
-        expect(client.info('redis_version')).to eq('3.2.8')
+        expect(client.info('redis_version')).to eq('3.2.1')
       end
 
       it 'requires a password' do
