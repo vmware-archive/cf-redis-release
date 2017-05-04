a1ca79d9b8ab893f18cfbb2172b2a1cfbd10d9e6
Tom Godkin <tgodkin@pivotal.io>
2017-05-04 16:48:41 +0100

    broker consumes IP and port from dedicated_node
    Removed unused config_command property from broker and dedicated_node
    links
    
    [#144192469]
    
    Signed-off-by: Dan Wendorf <github@danwendorf.com>

--- spec/unit/bosh_specs_spec.rb
+++ spec/unit/bosh_specs_spec.rb
@@ -8,7 +8,7 @@ describe 'bosh specs' do
   describe 'broker' do
     subject { YAML.load_file(BROKER_SPEC_PATH) }
 
-    it 'is configured expose redis.config_command as a bosh link' do
+    xit 'is configured expose redis.config_command as a bosh link' do
       expected_link = {
         'name' => 'redis_broker',
         'type' => 'redis',
@@ -17,16 +17,25 @@ describe 'bosh specs' do
 
       expect(subject['provides']).to include(expected_link)
     end
+
+    it 'is configured to consume dedicated_node' do
+      expected_consumer = {
+        'name' => 'dedicated_node',
+        'type' => 'redis',
+      }
+
+      expect(subject['consumes']).to include(expected_consumer)
+    end
   end
 
   describe 'dedicated-node' do
     subject { YAML.load_file(DEDICATED_NODE_SPEC_PATH) }
 
-    it 'is configured expose redis.config_command as a bosh link' do
+    it 'is configured to provide a link' do
       expected_link = {
         'name' => 'dedicated_node',
         'type' => 'redis',
-        'properties' => ['redis.config_command'],
+        'properties' => ['redis.broker.dedicated_port'],
       }
 
       expect(subject['provides']).to include(expected_link)
