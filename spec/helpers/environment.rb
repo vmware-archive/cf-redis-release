e80764cd6a6fd559e264b82e0d891ec51e5c391e
Emmanouil Kiagias <ekiagias@pivotal.io>
2017-07-18 16:21:33 +0100

    Removed unused execute_on function
    [#146276923]
    
    Signed-off-by: Tom Godkin <tgodkin@pivotal.io>

--- spec/helpers/environment.rb
+++ spec/helpers/environment.rb
@@ -151,16 +151,6 @@ module Helpers
       ).build(binding)
     end
 
-    def root_execute_on(ip, command)
-      root_prompt = '[sudo] password for vcap: '
-      root_prompt_length = root_prompt.length
-
-      output = ssh_gateway.execute_on(ip, command, root: true)
-      expect(output).not_to be_nil
-      expect(output).to start_with(root_prompt)
-      return output.slice(root_prompt_length, output.length - root_prompt_length)
-    end
-
     private
 
     def parse_host(raw_host)
