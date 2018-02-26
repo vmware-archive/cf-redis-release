f41b0746352026b411fd027c25e17833726bc0cc
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-02-26 16:01:26 +0000

    bump redis version to 4.0.8
    [#155431128]
    
    Signed-off-by: Panagiotis Xynos <panagiotis.xynos@engineerbetter.com>

--- spec/system/dedicated_plan_spec.rb
+++ spec/system/dedicated_plan_spec.rb
@@ -63,7 +63,7 @@ describe 'dedicated plan' do
 
       it 'runs correct version of redis' do
         client = service_client_builder(@binding)
-        expect(client.info('redis_version')).to eq('3.2.11')
+        expect(client.info('redis_version')).to eq('4.0.8')
       end
 
       it 'requires a password' do
