a6898adde8fbc3d41c785ebcdc92f14dc1e4097c
Maya Rosecrance <mrosecrance@pivotal.io>
2018-03-08 10:28:05 +0000

    Aof rewrite should fail if it runs longer than 2 min
    [#155434014]
    
    Signed-off-by: Panagiotis Xynos <panagiotis.xynos@engineerbetter.com>

--- jobs/cf-redis-broker/templates/drain.sh
+++ jobs/cf-redis-broker/templates/drain.sh
@@ -3,6 +3,8 @@
 set -e
 
 log_dir=/var/vcap/sys/log/cf-redis-broker
+REDIS_CLI_COMMAND=/var/vcap/packages/redis/bin/redis-cli
+
 
 disable_process_watcher() {
     echo "`date +%c` - Disabling process watcher" >> ${log_dir}/drain.log 2>&1
@@ -19,6 +21,25 @@ disable_process_watcher() {
 
     sleep 1
 }
+
+echo "`date +%c` - Starting AOF rewrites" >> ${log_dir}/drain.log 2>&1
+
+for redis_path in $(find /var/vcap/store/cf-redis-broker/redis-data -name redis.conf); do
+    REDIS_PASS="$(awk '$1 == "requirepass" {print $2}' ${redis_path})"
+    REDIS_PORT=$(awk '$1 == "port" {print $2}' "${redis_path}")
+    ${REDIS_CLI_COMMAND} -p ${REDIS_PORT} -a "${REDIS_PASS}" BGREWRITEAOF
+
+    timeout_timestamp=$(date "+%s" + 120)
+    until ${REDIS_CLI_COMMAND} -p ${REDIS_PORT} -a "${REDIS_PASS}" INFO | grep aof_rewrite_in_progress:0
+    do
+      if [ $timeout_timestamp -lt $(date "+%s") ]; then
+        echo "$(date): Redis at ${redis_path} failed to rewrite AOF file within 120 seconds" >> ${log_dir}/drain.log 2>&1
+        exit 1
+      fi
+
+      sleep 1
+    done
+done
 
 echo "`date +%c` - Starting drain" >> ${log_dir}/drain.log 2>&1
 
