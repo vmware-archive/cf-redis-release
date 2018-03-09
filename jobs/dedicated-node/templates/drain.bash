af9b163e097413337040ac0b71ceba6eb6fb7925
Panagiotis Xynos <panagiotis.xynos@engineerbetter.com>
2018-03-09 14:01:15 +0000

    Fix timeout timestamp calculation
    [#155434014]
    
    Signed-off-by: Ed Cook <ecook@pivotal.io>

--- jobs/dedicated-node/templates/drain.bash
+++ jobs/dedicated-node/templates/drain.bash
@@ -27,7 +27,7 @@ if [ -f "${PIDFILE}" ]; then
 
     ${REDIS_CLI_COMMAND} -p ${REDIS_PORT} -a "${REDIS_PASS}" BGREWRITEAOF
 
-    timeout_timestamp=$(date "+%s" + 120)
+    timeout_timestamp=$(($(date "+%s") + 120))
     until ${REDIS_CLI_COMMAND} -p ${REDIS_PORT} -a "${REDIS_PASS}" INFO | grep aof_rewrite_in_progress:0
     do
       if [ $timeout_timestamp -lt $(date "+%s") ]; then
