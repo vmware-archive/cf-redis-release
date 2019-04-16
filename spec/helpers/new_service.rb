3ad8e085b89bc7947705df78708705cb7409bc16
Lasse Berglund <lberglund@pivotal.io>
2019-04-16 16:45:41 +0100

    Refactor to please rubocop.

--- spec/helpers/new_service.rb
+++ spec/helpers/new_service.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 require 'helpers/plan'
 
@@ -8,28 +9,26 @@ module Helpers
       @name        = args.fetch(:name)
       @description = args.fetch(:description)
       @bindable    = !!args.fetch(:bindable)
-      @plans       = args.fetch(:plans).map { |p| Plan.new(p.merge(service_id: self.id)) }
+      @plans       = args.fetch(:plans).map { |p| Plan.new(p.merge(service_id: id)) }
     end
 
     attr_reader :id, :name, :description, :bindable, :plans
 
     def ==(other)
       is_a?(other.class) &&
-          id == other.id &&
-          name == other.name &&
-          description == other.description &&
-          bindable == other.bindable &&
-          plans == other.plans
+        id == other.id &&
+        name == other.name &&
+        description == other.description &&
+        bindable == other.bindable &&
+        plans == other.plans
     end
 
     def plan(plan_name)
-      plans.find { |p| p.name == plan_name } or
-          fail(PlanNotFoundError, [
-              %{Unknown plan with name: #{plan_name.inspect}},
-              "  Known plan names are: #{plans.map(&:name).inspect}"
-          ].join("\n")
-          )
+      plans.find { |p| p.name == plan_name } ||
+        raise(PlanNotFoundError, [
+          %(Unknown plan with name: #{plan_name.inspect}),
+          "  Known plan names are: #{plans.map(&:name).inspect}"
+        ].join("\n"))
     end
-
   end
 end
